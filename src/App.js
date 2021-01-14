78c4a4337647e5a2babb8247126f4a9d56032192
marisleidis84 <marisleidis84@gmail.com>
2021-01-14 14:10:18 -0300

    Card terminadas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 function App() {
   return (
-    <h1>hola mundo</h1>
+    <>
+      <div className="container">
+        <div className="row">
+        <div className="col-md-3">
+            <Card
+              img="https://th.bing.com/th/id/OIP.ZJxbzfYRBYzP0hmndbiJLgHaFj?pid=Api&rs=1"
+              title="Card title"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " 
+              buttonlabel="Find Out More!"/>
+          </div>
+          <div className="col-md-3">
+            <Card
+              img="https://th.bing.com/th/id/OIP.ZJxbzfYRBYzP0hmndbiJLgHaFj?pid=Api&rs=1"
+              title="Card title"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " 
+              buttonlabel="Find Out More!"
+              />
+          </div>
+          <div className="col-md-3">
+            <Card
+              img="https://th.bing.com/th/id/OIP.ZJxbzfYRBYzP0hmndbiJLgHaFj?pid=Api&rs=1"
+              title="Card title"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+              buttonlabel="Find Out More!"
+              />
+          </div>
+          <div className="col-md-3">
+            <Card
+              img="https://th.bing.com/th/id/OIP.ZJxbzfYRBYzP0hmndbiJLgHaFj?pid=Api&rs=1"
+              title="Card title"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+              buttonlabel="Find Out More!"
+              />
+          </div>
+
+        </div>
+      </div>
+
+    </>
   );
 }
 
